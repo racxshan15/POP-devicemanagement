b8d388799d0f26a84b20556ec14d655f8d4b3d52
Racxshan Nagalingam <510460@student.fontys.nl>
2023-05-24 14:52:13 +0200

    Versie 4 - toevoeging Platform en Serienummer

--- main.go
+++ main.go
@@ -22,9 +22,10 @@ func HTMLpagina(writer http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case "GET":
 		http.ServeFile(writer, request, "C:\\Users\\racxs\\OneDrive\\Documenten\\Infrastructure\\Semester 2\\Pop\\web.html") // Geeft het pad naar het HTML-bestand aan dat moet worden weergegeven.
+
 	case "POST":
 		if err := request.ParseForm(); err != nil {
-			fmt.Fprintf(writer, "ParseForm() err: %v", err)
+			errors("Kon niet verbinden met de poort", err, 6)
 			return
 		}
 		fmt.Fprintf(writer, "Post from website! r.PostFrom = %v\n", request.PostForm)
@@ -33,6 +34,8 @@ func HTMLpagina(writer http.ResponseWriter, request *http.Request) {
 		apparaatnaam := request.FormValue("apparaatnaam")
 		voornaam := request.FormValue("name")
 		achternaam := request.FormValue("surname")
+		platform := request.FormValue("platform")
+		serienummer := request.FormValue("serienummer")
 		geldig_tot, _ := time.Parse("2006-01-02", request.FormValue("datum")) // Parse de waarde van 'datum' naar een tijd.Time object.
 
 		// Maak verbinding met de database
@@ -44,7 +47,7 @@ func HTMLpagina(writer http.ResponseWriter, request *http.Request) {
 		defer db.Close()
 
 		// Voeg gegevens toe aan de database
-		_, err = db.Exec("INSERT INTO toekenningen (voornaam, achternaam, apparaat, geldig_tot, toekenning_datum) VALUES (?, ?, ?, ?, NOW())", voornaam, achternaam, apparaatnaam, geldig_tot)
+		_, err = db.Exec("INSERT INTO toekenningen (voornaam, achternaam, apparaat, geldig_tot, platform, serienummer, toekenning_datum) VALUES (?, ?, ?, ?, ?, ?, NOW())", voornaam, achternaam, apparaatnaam, geldig_tot, platform, serienummer)
 		if err != nil {
 			fmt.Fprintf(writer, "Niet gelukt om gegevens in database te stoppen: %v", err)
 			return
